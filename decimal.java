3069050ed1c8fbb84c57d0e0de76463b4d669dde
gaurshir <136344651+gaurshir@users.noreply.github.com>
2023-06-12 19:32:54 +0530

    for testing purpose to review

---
+++ decimal.java
@@ -0,0 +1,33 @@
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+
+public class decimal {
+
+
+    public static void main(String args[]) {
+
+        Map<String, Object> map =  new HashMap<>();
+        map.put("test", true);
+        Object test1 = "true";
+        Object test = (String) test1.toString();
+
+        Object flag = map.get("test");
+
+        if(flag == null) {
+            System.out.println("null");
+        } else {
+            String h =  flag.toString();
+            System.out.println(flag.toString());
+
+        }
+
+        System.out.print(test);
+
+      }
+
+}
